ad6a96fc34fad80b5f768c47ae96bd5129fc830d
vuongquocanh973-cell <vuongquocanh973@gmail.com>
2026-04-30 02:52:55 +0700

    Simplify PostgreSQL connection

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,28 +1,26 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using UrlShortener.Data;
 using UrlShortener.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 string connectionString;
 
 if (databaseUrl != null)
 {
-    var uri = new Uri(databaseUrl);
-    var userInfo = uri.UserInfo.Split(':');
-    var database = uri.AbsolutePath.TrimStart('/').Split('?')[0];
-    var password = Uri.UnescapeDataString(userInfo[1]);
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={database};Username={userInfo[0]};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+    connectionString = databaseUrl;
+}
 else
 {
-    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
 }
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
+
 builder.Services.AddScoped<UrlShortenerService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -39,9 +37,11 @@ app.UseSwaggerUI();
 app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
+
 app.Run();
