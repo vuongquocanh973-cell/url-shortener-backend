363b2671f4a2e1b4c28f59a9d0bb1117fe43053c
vuongquocanh973-cell <vuongquocanh973@gmail.com>
2026-04-30 01:49:46 +0700

    Switch to PostgreSQL for production

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using UrlShortener.Data;
 using UrlShortener.Services;
 
@@ -6,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<UrlShortenerService>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,4 +24,9 @@ app.UseSwaggerUI();
 app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 app.Run();
