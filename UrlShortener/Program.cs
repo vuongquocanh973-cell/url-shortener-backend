837c31e6c76f4edb6cc61d40d27aa286916a3348
vuongquocanh973-cell <vuongquocanh973@gmail.com>
2026-04-30 02:37:51 +0700

    Fix connection string SSL parsing

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -13,8 +13,9 @@ if (databaseUrl != null)
 {
     var uri = new Uri(databaseUrl);
     var userInfo = uri.UserInfo.Split(':');
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
-}
+    var database = uri.AbsolutePath.TrimStart('/').Split('?')[0];
+    var password = Uri.UnescapeDataString(userInfo[1]);
+    connectionString = $"Host={uri.Host};Port={uri.Port};Database={database};Username={userInfo[0]};Password={password};SSL Mode=Require;Trust Server Certificate=true";
 else
 {
     connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
