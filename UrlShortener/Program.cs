4033c3fdc1dacb59d4e829b0dcad76afe0841e16
vuongquocanh973-cell <vuongquocanh973@gmail.com>
2026-05-05 02:38:58 +0700

    Fix port parsing in connection string

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -11,11 +11,14 @@ string connectionString;
 
 if (databaseUrl != null && databaseUrl.StartsWith("postgresql://"))
 {
-    var uri = new Uri(databaseUrl);
+    
+    var cleanUrl = databaseUrl.Split('?')[0];
+    var uri = new Uri(cleanUrl);
     var userInfo = uri.UserInfo.Split(':');
     var database = uri.AbsolutePath.TrimStart('/');
     var password = Uri.UnescapeDataString(userInfo[1]);
-    connectionString = $"Host={uri.Host};Port={uri.Port};Database={database};Username={userInfo[0]};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+    var port = uri.Port > 0 ? uri.Port : 5432;
+    connectionString = $"Host={uri.Host};Port={port};Database={database};Username={userInfo[0]};Password={password};SSL Mode=Require;Trust Server Certificate=true";
 }
 else
 {
