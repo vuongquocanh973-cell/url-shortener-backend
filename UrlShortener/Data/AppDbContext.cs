527c89ad70c4453da84287966c91c281fa802ea5
vuongquocanh973-cell <vuongquocanh973@gmail.com>
2026-05-05 03:15:20 +0700

    Fix table name to lowercase for PostgreSQL

--- UrlShortener/Data/AppDbContext.cs
+++ UrlShortener/Data/AppDbContext.cs
@@ -11,6 +11,6 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<ShortUrl>().ToTable("ShortUrls");
+        modelBuilder.Entity<ShortUrl>().ToTable("shorturls");
     }
 }
